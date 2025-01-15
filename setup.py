617b3aa512d6a96cd0e26d355e13742b828ad748
oskarkaluziak <oskar@kaluziak.pl>
2025-01-15 19:20:27 +0100

    setup for exe building

--- setup.py
+++ setup.py
@@ -26,5 +26,5 @@ setup(
     version="1.0",
     description="POMOKA",
     options={"build_exe": build_exe_options},
-    executables=[Executable("pomoka.py", base=None, icon="images/icon.ico")],
+    executables=[Executable("pomoka.py", base="Win32GUI", icon="images/icon.ico")],
 )
